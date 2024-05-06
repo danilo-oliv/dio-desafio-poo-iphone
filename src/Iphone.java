7a5212ca6a727d59a857a63ee861df0a939ba6c2
danilo <daniloborges@unipam.edu.br>
2024-05-06 12:37:49 -0300

    feat: diagrama e implementação iphone

---
+++ src/Iphone.java
@@ -0,0 +1,56 @@
+public class Iphone implements ReprodutorMusical, NavegadorInternet, AparelhoTelefonico {
+
+    @Override
+    public void ligar() {
+        System.out.println("Ligando...");
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Ligação atendida.");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+
+    @Override
+    public void exibirPagina() {
+        System.out.println("Exibindo página.");
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada.");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Página atualizada.");
+    }
+
+    @Override
+    public void tocar() {
+        System.out.println("Tocando música...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    @Override
+    public void selecionarMusica() {
+        System.out.println("Música selecionada.");
+    }
+
+    public static void main(String[] args) {
+        Iphone iphone15 = new Iphone();
+        iphone15.ligar();
+        iphone15.adicionarNovaAba();
+        iphone15.selecionarMusica();
+    }
+
+}
+
